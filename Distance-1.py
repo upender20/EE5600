ed898643e0fd95eb78f56d15b5b8b61b6fc43628
upender20 <70749122+upender20@users.noreply.github.com>
2020-09-10 23:27:33 +0530

    Add files via upload

---
+++ Distance-1.py
@@ -0,0 +1,21 @@
+import sympy as sp
+
+from sympy import Eq,solve,symbols
+
+import math 
+
+[a,b,c]=[-1,-5,-10]
+
+l,x,y,z=symbols('l,x,y,z')
+
+[x,y,z]=[2+3*l,-1+4*l,2+2*l]
+
+r=solve(Eq(x-y+z-5,0),l)
+
+[x,y,z]=[2+3*r[0],-1+4*r[0],2+2*r[0]]
+
+d=math.sqrt(pow(x-a,2)+pow(y-b,2)+pow(z-c,2))
+
+print('Distance between point of intersection of line and plane with given point',d)
+
+print('POI',x,y,z)
